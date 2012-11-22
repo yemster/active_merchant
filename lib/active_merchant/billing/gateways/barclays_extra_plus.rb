12269f40ef36803e1f6a08ce7b91ec1b191f6380
dmgarland <dan@dangarland.co.uk>
2012-11-22 12:27:40 +0000

    Adding documenation to the Gateway class

--- lib/active_merchant/billing/gateways/barclays_extra_plus.rb
+++ lib/active_merchant/billing/gateways/barclays_extra_plus.rb
@@ -1,5 +1,31 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
+    # = Barclays ExtraPlus / DirectLink Gateway
+    #
+    # Barclays ePDQ is being phased out in favour of a product based on the Ogone
+    # DirectLink Gateway, which is being marketed as "Barclays Extra Plus". It 
+    # is the API version of the platform, as opposed to the e-commerce hosted 
+    # pages of Barclays Extra Plus.
+    #
+    # This class is based upon the OgoneGateway class as it re-uses the same 
+    # architecture, with different endpoints. Please refer to the OgoneGateway 
+    # class for more information.
+    #
+    # == Setup Notes
+    #
+    # In order to enable API access with BarclaysExtraPlus, an API user 
+    # must be created in the Barclays e-commerce backend. This user must have 
+    # admin rights, and the checkbox "Special user for API (no access to admin.)"
+    #
+    # In order to use the ALIAS feature to store authorisations without 
+    # storing card details, this feature must be enabled via a support ticket 
+    # with Barclays. Then, the ALIAS variable must be added to the list of dynamic
+    # parameters on the Technical Information > Transaction Feedback tab, and
+    # a SHAOUT signature must be provided. 
+    #
+    # All the unit and remote tests passed with a BarclaysExtraPlus test account
+    # on November 22nd, 2012.     
+    #
     class BarclaysExtraPlusGateway < OgoneGateway
       
       BASE_URLS = {
